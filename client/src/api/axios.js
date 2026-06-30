530fe6b4dc2361406f700e88dc97f84aa1263521
Francis Gerald Lopez <lopez.francisgerald.p@lyceum.edu.ph>
2026-06-30 22:39:02 +0800

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -17,12 +17,11 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response?.status === 401) {
+    const isLoginRequest = err.config?.url?.includes('/login');
+    if (err.response?.status === 401 && !isLoginRequest) {
       localStorage.removeItem('token');
       window.location.href = '/login';
     }
     return Promise.reject(err);
   }
 );
-
-export default api;
